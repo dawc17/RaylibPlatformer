852a8c0a5405be69f0bed9e52328bded66e9356f
daw_c17 <100421659+dawc17@users.noreply.github.com>
2025-10-22 15:49:56 +0200

    Add Raylib window setup and gradient rectangle
    Set up a basic Raylib window with defined screen size, title, and FPS. Added a rotating variable (currently unused) and replaced the previous text drawing with a horizontal gradient rectangle. Also added C/C++ IntelliSense configuration for VSCode and included a new image resource.

--- main.c
+++ main.c
@@ -1,15 +1,30 @@
 #include "raylib.h"
+#include <math.h>
+#include "rlgl.h"
 
-int main(void) {
-  InitWindow(1280, 720, "Window");
+int main(void)
+{
+  const int SCREEN_WIDTH = 800;
+  const int SCREEN_HEIGHT = 450;
+  const char *TITLE = "Raylib Program";
+  int FPS = 60;
 
-  while (!WindowShouldClose()) {
+  InitWindow(SCREEN_WIDTH, SCREEN_HEIGHT, TITLE);
+
+  float rotation = 0.0f;
+
+  SetTargetFPS(FPS);
+
+  while (!WindowShouldClose())
+  {
+    rotation += 0.2f;
     BeginDrawing();
     ClearBackground(RAYWHITE);
-    DrawText("hallo ben du er svart", 180, 200, 60, BLACK);
+
+    DrawRectangleGradientH(SCREEN_WIDTH / 4 * 2 - 90, 170, 180, 130, MAROON, GOLD);
+
     EndDrawing();
   }
-
   CloseWindow();
 
   return 0;
